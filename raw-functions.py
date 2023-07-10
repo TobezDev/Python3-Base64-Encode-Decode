faccc690057df4bb30ba2b8d3bb31d9b0ec5a553
TobezDev <admin@tobez.dev>
2023-07-10 14:29:48 +0100

    update global

--- raw-functions.py
+++ raw-functions.py
@@ -1,32 +1,5 @@
-# Py3 (Installer)
+# __global__ Functions
 
-def encode_base64string(bin_fractals, padding):
-    base64_string = [CHARACTERS[int(bin_fractal, 2)] for bin_fractal in bin_fractals]
-    for i in range(len(base64_string) - padding, len(base64_string)):
-        base64_string[i].replace("A", "=")
-    return "".join(base64_string)
-
-def decode_base64string(base64_string):
-    padding = base64_string.count("=")
-    base64_string = base64_string.replace("=", "A")
-    bin_fractals = [str(bin(CHARACTERS.index(char)))[2:].rjust(6, "0") for char in base64_string]
-    return bin_fractals, padding
-
-# Py3 (Microsodt Store)
-
-def encode_base64string(bin_fractals, padding):
-    base64_string = [CHARACTERS[int(bin_fractal, 2)] for bin_fractal in bin_fractals]
-    for i in range(len(base64_string) - padding, len(base64_string)):
-        base64_string[i].replace("A", "=")
-    return "".join(base64_string)
-
-def decode_base64string(base64_string):
-    padding = base64_string.count("=")
-    base64_string = base64_string.replace("=", "A")
-    bin_fractals = [str(bin(CHARACTERS.index(char)))[2:].rjust(6, "0") for char in base64_string]
-    return bin_fractals, padding
-
-# Py2 (Global)
 
 def encode_base64string(bin_fractals, padding):
     base64_string = [CHARACTERS[int(bin_fractal, 2)] for bin_fractal in bin_fractals]
